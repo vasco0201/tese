a5ddd6fd7c0142487d53aa68f6ff0cf5b7b065e4
vasco0201 <vasco_leal_500@hotmail.com>
2019-07-11 15:59:01 +0100

    added changes to merge_files

--- merge_files.py
+++ merge_files.py
@@ -3,10 +3,14 @@ import sys
 import os
 import pandas as pd
 files = []
-for i in sys.argv[1:]:
-	files.append(pd.read_csv(i))
+
+if (len(sys.argv))>1:
+	for i in sys.argv[1:]:
+		files.append(pd.read_csv(i))
+else:
+	files = [pd.read_csv('/home/vasco/tese/CT15Mn-111018-150419/dados_camara.csv'),pd.read_csv('/home/vasco/tese/CT15Mn-150818_101018/dados_camara.csv'), pd.read_csv('/home/vasco/tese/Espiras/InfoCentral_20180119_1803/dados_camara.csv')]
 combined_csv = pd.concat(files)
 #export to csv
-combined_csv.to_csv("combined_csv.csv", sep= ',', index=False)
+combined_csv.to_csv("dados_camara_todos.csv", sep= ',', index=False)
 
 
